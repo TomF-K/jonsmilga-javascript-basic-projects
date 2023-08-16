e65c68ff7de13b58b5c8b2a4b7e116e3d9946c53
TomF-K <111983192+TomF-K@users.noreply.github.com>
2023-08-16 12:22:40 +0100

    targeted toggle button in js

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,8 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+// target button
+
+const toggleButton = document.querySelector(".nav-toggle");
+console.log(toggleButton);
