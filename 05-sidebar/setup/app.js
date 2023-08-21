255c7dc65e9c816a5c1c7092ef2a8c0b057490de
TomF-K <111983192+TomF-K@users.noreply.github.com>
2023-08-21 10:32:21 +0100

    added event listeners to complete sidebar toggle

--- 05-sidebar/setup/app.js
+++ 05-sidebar/setup/app.js
@@ -5,3 +5,15 @@
 const sidebarToggle = document.querySelector(".sidebar-toggle");
 const sidebar = document.querySelector(".sidebar");
 const closeBtn = document.querySelector(".close-btn");
+
+// add event listener to sidebar-toggle
+
+sidebarToggle.addEventListener("click", function () {
+  sidebar.classList.add("show-sidebar");
+});
+
+// add class toggle to close-btn
+
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
