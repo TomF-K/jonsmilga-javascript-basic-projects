83c24d045f2eb6d0bfd829fced959e477d187dd5
TomF-K <111983192+TomF-K@users.noreply.github.com>
2023-08-21 10:28:26 +0100

    selected sidebar elements in app.js

---
+++ 05-sidebar/setup/app.js
@@ -0,0 +1,7 @@
+// select elements
+
+// select elements
+
+const sidebarToggle = document.querySelector(".sidebar-toggle");
+const sidebar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector(".close-btn");
