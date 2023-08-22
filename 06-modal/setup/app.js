ba97cbcad4e159accb99cd781d2a419009425baf
TomF-K <111983192+TomF-K@users.noreply.github.com>
2023-08-22 14:57:50 +0100

    reordered app.js

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -6,14 +6,14 @@ const closeBtn = document.querySelector(".close-btn");
 
 // listen for click events on modal-btn and close-btn
 
+// when user clicks modal-btn add .open-modal to modal-overlay
+
 modalBtn.addEventListener("click", function () {
   modalOverlay.classList.add("open-modal");
 });
 
+// when user clicks close-btn remove .open-modal from modal-overlay
+
 closeBtn.addEventListener("click", function () {
   modalOverlay.classList.remove("open-modal");
 });
-
-// when user clicks modal-btn add .open-modal to modal-overlay
-
-// when user clicks close-btn remove .open-modal from modal-overlay
