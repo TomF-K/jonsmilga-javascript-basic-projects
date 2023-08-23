4956469a11f12d2337215008c012b84126448fb8
TomF-K <111983192+TomF-K@users.noreply.github.com>
2023-08-23 10:41:44 +0100

    completed DOM traversing exercise

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -24,6 +24,7 @@ const btns = document.querySelectorAll(".question-btn");
 
 btns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
-    console.log(e.currentTarget.parentElement);
+    const question = e.currentTarget.parentElement.parentElement;
+    question.classList.toggle("show-text");
   });
 });
