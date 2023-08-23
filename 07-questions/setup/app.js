a79082cabd3f881abba9c3af8b8466f68c2b555f
TomF-K <111983192+TomF-K@users.noreply.github.com>
2023-08-23 10:37:25 +0100

    started JS setup for traversing DOM

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,29 @@
 //using selectors inside the element
+
+// select question text
+// add click event to plus icon
+// add show-text class
+
+// const question = document.getElementsByClassName(".question");
+// const plusBtn = document.querySelector(".plus-icon");
+// const minusBtn = document.querySelector(".minus-icon");
+
+// plusBtn.addEventListener("click", function () {
+//  question.classList.add("show-text");
+// });
+
+// minusBtn.addEventListener("click", function () {
+//  question.classList.remove("show-text");
+// });
+
 // traversing the dom
+
+// select all buttons, loop over them
+
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    console.log(e.currentTarget.parentElement);
+  });
+});
