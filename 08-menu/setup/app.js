1614e944bc5c2e152216e2d29faf2c699065d35c
TomF-K <111983192+TomF-K@users.noreply.github.com>
2023-08-24 10:17:03 +0100

    initial setup of JS logic for mapping menu array

--- 08-menu/setup/app.js
+++ 08-menu/setup/app.js
@@ -72,3 +72,12 @@ const menu = [
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
+
+const sectionCenter = document.querySelector(".section-center");
+
+window.addEventListener("DOMContentLoaded", function () {
+  let displayMenu = menu.map(function (item) {
+    return `<h1>hello world</h1>`;
+  });
+  console.log(displayMenu);
+});
