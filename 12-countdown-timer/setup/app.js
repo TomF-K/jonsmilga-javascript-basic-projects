5b7f7dd7bc6a26f24b27e57f29c55978798a4027
TomF-K <111983192+TomF-K@users.noreply.github.com>
2023-09-14 14:00:46 +0100

    logic setup for countdown timer project

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -27,3 +27,22 @@ const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 // target all h4 (hours/days etc)
 const items = document.querySelectorAll(".deadline-format h4");
+
+let futureDate = new Date(2023, 11, 31, 23, 59, 59);
+
+// easy values to get
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+// harder values to access
+
+let month = futureDate.getMonth();
+month = months[month];
+
+const date = futureDate.getDate();
+
+// update year, hours, and mins in HTML
+
+giveaway.textContent = `giveaway ends on ${date} ${month} ${year} ${hours}:${minutes}`;
